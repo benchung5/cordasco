71fa5003982d23a73db43221e1d543df637905fe
Ben Chung <ben@benchung.com>
2019-06-17 21:36:57 -0400

    first commit

---
+++ treatments-available.php
@@ -0,0 +1,96 @@
+<?php include_once 'header.php'; ?>
+
+<div class="loading-container">
+    <div class="body-container">
+        <div class="row">
+            <div class="large-9 columns" id="content">
+                <!--content-->
+
+                <h1>Treatments Available</h1>
+
+                <div id="page-content">
+                    <div class="row">
+                        <div class="large-6 columns">
+                            <h3>Theralase 3D 660 nm</h3>
+                            <ul>
+                                <li>Anti-Inflammation
+                                <li>Anti-Pain (Analgesic)
+                                <li>Accelerated Tissue Repair and Cell Growth
+                                <li>Improved Vascular Activity
+                                <li>Increased Metabolic Activity
+                                <li>Trigger Points and Acupuncture Points
+                                <li>Reduced Fibrous Tissue Formation
+                                <li>Improved Nerve Function
+                                <li>Immunoregulation
+                                <li>Faster Wound Healing
+                            </ul>
+                        </div>    
+                        <div class="large-6 columns">
+                            <iframe width="400px" height="300" src="https://www.youtube.com/embed/e6kkOf0hK9c" frameborder="0" allowfullscreen></iframe>
+                            <div class="spacer"></div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="large-6 columns">
+                            <h3>Decompression Therapy</h3>
+                            Peripheral radiation/sciatica and pain associated with:
+                            <ul>
+                                <li>Bulging/Herniated discs
+                                <li>Degenerative Disc Disease
+                                <li>Spinal root impingement
+                                <li>Hypomobility
+                                <li>Degenerative Joint Disease
+                                <li>Facet Syndrome
+                                <li>Compression Fractures
+                                <li>Joint Pain
+                            </ul>
+                        </div>    
+                        <div class="large-6 columns">
+                            <iframe width="400px" height="300" src="https://www.youtube.com/embed/biWzYPUtP1Q" frameborder="0" allowfullscreen></iframe>
+                            <div class="spacer"></div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="large-6 columns">
+                            <h3>Shockwave Therapy</h3>
+                            Treatable Conditions:
+                            <ul>
+                                <li>Plantar Fasciitis
+                                <li>Achilles Tendinopathy 
+                                <li>Scar Tissue Treatment 
+                                <li>Tennis Elbow 
+                                <li>Jumpers Knee 
+                                <li>Stress Fractures 
+                                <li>Non Healing Ulcers 
+                                <li>Calcific Rotator Cuff Tendinitis 
+                            </ul>
+                            Benefits:
+                            <ul>
+                                <li>Non surgical treatment
+                                <li>No side effects
+                                <li>Accelerates healing
+                                <li>Affordable
+                                <li>Coverage available from most insurance companies      
+                            </ul>
+                        </div>    
+                        <div class="large-6 columns">
+
+                            <div class="spacer"></div>
+                        </div>
+                    </div>
+                </div>
+                <!--end content-->
+            </div>
+            <div class="large-3 columns" id="sidebar">
+                <!--sidebar-->
+                <?php
+                include_once 'sidebar.php';
+                ?>
+                <!--end sidebar-->
+            </div>
+        </div>
+        <div class="clear"></div>
+    </div>
+</div>
+
+<?php include_once 'footer.php'; ?>
